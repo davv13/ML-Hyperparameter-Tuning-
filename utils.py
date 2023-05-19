95f73866dbb0062b396a0c84d647641f579f9622
davitdavtyan969 <david.davtyan96@gmail.com>
2023-05-19 19:40:26 +0400

    First Commit

---
+++ utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import xlrd
+from sklearn.model_selection import train_test_split 
+import xgboost as xgb 
+from sklearn.metrics import accuracy_score
+
+
+
+
